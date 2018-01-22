857ba5be63936611e438dd71e879ad9ef6c4ab9b
Maurilyn <maurilyn@gmail.com>
2018-01-22 13:13:51 +0000

    Desc Pag Inicial

--- con.php
+++ con.php
@@ -2,10 +2,11 @@
 
     header("Content-type: text/html; charset=iso-8859-1");
 
+    // 127.0.0.1  
     // mysql785.umbler.com
-    $host = "127.0.0.1";
+    $host = "mysql785.umbler.com";
     $user = "maurilyn";             //Your Cloud 9 username
-    $pass = "";                     //Remember, there is NO password by default!
+    $pass = "gtur5182";                     //Remember, there is NO password by default!
     $db = "gtur";                   //Your database name you want to connect to
     $port = 3306;                   //The port #. It is always 3306
     
--- index.php
+++ index.php
@@ -20,12 +20,18 @@
                         </div>
                         <div class="row">
                             <div class="col-md-10 offset-md-1">
-                                <div class="alert alert-info text-center" role="alert">
+                                <div class="alert alert-light text-center" role="alert">
                                     <h1 style="font-size:30px;">GuaruTur, o mais completo site de informa&ccedil;&otilde;es tur&iacute;sticas de Guaruj&aacute; - SP</h1>
                                 </div>
                             </div>
                         </div>
                         <div class="row">
+                            <div class="col-md-5 text-justify">
+                                <h2 class="text-center" style="font-size:25px;">Guaruj&aacute; A P&eacute;rola do Atl&acirc;ntico</h2>
+                                <p style="text-indent: 1.5em; font-size:15px;">Famosa pelas suas belezas naturais, praias que cercam a regi&atilde;o e por seu potencial tur&iacute;stico, &eacute; considerada uma das cidades mais bonitas do litoral, conhecida tamb&eacute;m como "P&eacute;rola do Atl&acirc;ntico".</p>
+                                <p style="text-indent: 1.5em; font-size:15px;">Possui 27 praias de todos os tamanhos, de v&aacute;rias cores, de &aacute;guas limpas e mar aberto, cada uma com sua particularidade, iluminadas pelo sol e sorriso das crian&ccedil;as e dos surfistas. Al&eacute;m disso, uma delas, a Praia do Tombo, &eacute; a &uacute;nica do Brasil a receber o selo internacional Bandeira Azul. Ao longo dos 22 km das praias guarujaenses, voc&ecirc; encontrar&aacute; trilhas selvagens para praias paradis&iacute;acas, como as praias de Cheira-Lim&atilde;o ou Cambur&iacute;, praias bem movimentadas e de beleza sem igual, como as praias da Pitangueiras ou Enseada, praias distantes, pouco frequentadas e de f&aacute;cil acesso, como &eacute;den e Mar Casado.</p>
+                                <p style="text-indent: 1.5em; font-size:15px;">Mas Guaruj&aacute; oferece muito mais: Trilhas que contam hist&oacute;ria e emocionam; Mirantes com vista para cen&aacute;rios realmente vibrantes; Ilhas rodeadas por &aacute;guas multicoloridas; Fortes, Fortalezas e Pavilh&otilde;es que fazem sentir a nostalgia do passado e encantam tamb&eacute;m pelos personagens que deixaram pegadas na hist&oacute;ria.</p>
+                            </div>
                             <div class="col-md-7">
                                 <div id="carouselExampleIndicators" class="carousel slide sombra" data-ride="carousel">
                                     <ol class="carousel-indicators">
@@ -46,7 +52,7 @@
                                             </div>
                                         </div>
                                         <div class="carousel-item">
-                                            <img class="d-block w-100" src="rs/img/SlideAsturias.jpg" class="img-fluid" alt="Praia das Astúrias">
+                                            <img class="d-block w-100" src="rs/img/SlideAsturias.jpg" class="img-fluid" alt="Praia das Ast&uacute;rias">
                                             <div class="carousel-caption d-none d-md-block">
                                                 <h2>Ast&uacute;rias</h2>
                                                 <p>Praia badalada, bonita e boa para banho.</p>
@@ -74,16 +80,10 @@
                                     </a>
                                     <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
                                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                        <span class="sr-only">Próximo</span>
+                                        <span class="sr-only">Pr&oacute;ximo</span>
                                     </a>
                                 </div>                    
                             </div>
-                            <div class="col-md-5">
-                                <iframe class="sombra" width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyASyT70pbnOqbTVWFnMG17yDFzw_kWZXNo&q=Guarujá+SP" allowfullscreen></iframe>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <hr>
                         </div>
                         <div class="row">
                             <div class="col-md-3">
